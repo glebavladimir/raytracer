0840b6ea87009ca9d88b8bc0057fa8a0094cc99e
Vladimir Gleba <gleba.vladimir@gmail.com>
2022-08-25 00:03:29 +0300

    Simplify color blending

--- src/Render/Image/Ppm/PixelFactory.php
+++ src/Render/Image/Ppm/PixelFactory.php
@@ -7,17 +7,15 @@ use App\Vector\Color255;
 
 class PixelFactory implements PixelFactoryInterface
 {
-    const BLEND_WHITE_COEFFICIENT = 1.8;
-    const BLEND_GREY_COEFFICIENT = 1.5;
+    const SATURATION_COEFFICIENT = .3;
 
     private Color255 $baseColor;
 
     public function fromCoefficient(float $coefficient): Pixel
     {
         return new Pixel(
-            $this->getBaseColor()
-                ->blend(Color255::getWhiteColor()->mul($coefficient), self::BLEND_WHITE_COEFFICIENT)
-                ->blend(Color255::getLightGreyColor(), self::BLEND_GREY_COEFFICIENT)
+            Color255::getWhiteColor()->mul($coefficient)
+                ->blend(Color255::getDarkRedColor(), 1/self::SATURATION_COEFFICIENT)
         );
     }
 
